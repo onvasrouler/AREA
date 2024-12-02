00f00df2aa0706a060acd6262c7ceb67c79389b0
Klayni <klayni.milandou@epitech.eu>
2024-12-02 20:37:13 +0100

    :lock: Use environment variable for API url

--- front/src/app/APIClient.js
+++ front/src/app/APIClient.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.ENV_API_URL;
 
 const callApi = async (endpoint, method = "GET", body = null) => {
   const token = sessionStorage.getItem("token");
