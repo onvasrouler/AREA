9acb83b844f4cf983ab19f780f7033cdc1f8d973
aimericdrk <aimeric.rouyer@epitech.eu>
2024-12-03 14:23:06 +0100

    :lock: adding the possibility to activate or desactivate test endpoint

--- backend/src/router/user/user.query.js
+++ backend/src/router/user/user.query.js
@@ -1,8 +1,10 @@
 const useractions = require("./user.js");
 const checkAuthenticated = require("../../middleware/auth.js");
+const enableTestEndpoint = process.env.ENABLE_TEST_ENDPOINT == "true"
 
 module.exports = function (app) {
-    app.post("/fastregister", useractions.fastregister);
+    if (enableTestEndpoint)
+        app.post("/fastregister", useractions.fastregister);
 
     app.post("/register", useractions.register);
     app.post("/register/verify", useractions.verifyregister);
@@ -14,7 +16,8 @@ module.exports = function (app) {
     app.post("/logout", checkAuthenticated, useractions.logout);
     app.post("/logouteverywhere", checkAuthenticated, useractions.logouteverywhere);
 
-    app.delete("/fastprofile", checkAuthenticated, useractions.deletefastprofile);
+    if (enableTestEndpoint)
+        app.delete("/fastprofile", checkAuthenticated, useractions.deletefastprofile);
 
     app.delete("/profile", checkAuthenticated, useractions.deleteaccount);
     app.delete("/profile/confirm", checkAuthenticated, useractions.confirmdeleteaccount);
