d7480d58b54d601b4cc43f7a78686fb7b5522b72
Klayni <klayni.milandou@epitech.eu>
2024-12-04 23:46:02 +0100

    :sparkles: Add dashboard page to routes

--- front/src/app/Routes.jsx
+++ front/src/app/Routes.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { DashboardPage } from "../features/dashboard/DashboardPage";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
