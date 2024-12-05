5feded26327fd1590574c25db46186c9d9e154c2
aimericdrk <aimeric.rouyer@epitech.eu>
2024-12-05 23:24:27 +0100

    :sparkles: remove debug code + possibility to list user's pr and repo

--- backend/src/router/api/api.js
+++ backend/src/router/api/api.js
@@ -8,10 +8,13 @@ exports.profile = async (req, res) => {
     try {
         if (!req.user || req.user == null) // if the user is not logged in
             return api_formatter(req, res, 401, "notloggedin", "you are not logged in", null, null, null); // return a 401 error
+
         const user_infos = { // this will store the user informations
             "username": req.user.username,
             "email": req.user.email,
             "account_type": req.user.accountType,
+            "logged_in_discord": req.user.discord_token ? true : false,
+            "logged_in_github": req.user.github_token ? true : false,
         };
         return api_formatter(req, res, 200, "success", "you are authenticated", user_infos, null, null); // return the user informations
     } catch (err) { // if an error occured
@@ -30,7 +33,6 @@ exports.inviteDiscordBot = async (req, res) => {
 // This function will return the discord server where the user is admin
 exports.getDiscordServer = async (req, res) => {
     try {
-        console.log(req.guilds);
         const matchingGuilds = req.guilds.map(guild => ({
             id: guild.id,
             name: guild.name,
@@ -61,5 +63,40 @@ exports.getListOfChannels = async (req, res) => {
         return api_formatter(req, res, 200, "success", "you are authenticated", channelsList, null, null); // return the user informations
     } catch (err) {
         return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord server", null, err, null);
+    }
+}
+
+exports.getPullRequests = async (req, res) => {
+    try {
+        req.user = await UserModel.findOne({ email: "aimeric.rouyer@gmail.com" });
+        const token = req.user.github_token.access_token;
+        if (!token)
+            return api_formatter(req, res, 401, "notloggedin", "you are not logged in using github", null, null, null);
+        const prsResponse = await axios.get(
+            "https://api.github.com/search/issues?q=type:pr+author:@me",
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            }
+        );
+        return api_formatter(req, res, 200, "success", "your github pull requests have been fetched with success", prsResponse.data, null, null); // return the user informations
+    } catch (error) {
+        return api_formatter(req, res, 500, "error", "An error occured while trying to get the github pull requests", null, error, null);
+    }
+}
+
+exports.getMyRepos = async (req, res) => {
+    try {
+        const token = req.user.github_token.access_token;
+        if (!token)
+            return api_formatter(req, res, 401, "notloggedin", "you are not logged in using github", null, null, null);
+        const reposResponse = await axios.get(
+            "https://api.github.com/user/repos",
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            }
+        );
+        return api_formatter(req, res, 200, "success", "your github repository have been fetched with success", reposResponse.data, null, null); // return the user informations
+    } catch (error) {
+        return api_formatter(req, res, 500, "error", "An error occured while trying to get the github repositories", null, error, null);
     }
 }
--- backend/src/router/api/api.query.js
+++ backend/src/router/api/api.query.js
@@ -6,7 +6,10 @@ module.exports = function (app) {
     app.get("/profile_info", checkAuthenticated, api_interactions.profile);
     app.get("/invite_discord_bot", api_interactions.inviteDiscordBot);
 
-    app.get("/get_my_discord_server", discordAuth, api_interactions.getDiscordServer);
-    app.get("/get_list_of_channels", discordAuth, api_interactions.getListOfChannels);
+    app.get("/get_my_discord_server", checkAuthenticated, discordAuth, api_interactions.getDiscordServer);
+    app.get("/get_list_of_channels", checkAuthenticated, discordAuth, api_interactions.getListOfChannels);
+
+    app.get("/get_pull_requests", checkAuthenticated, api_interactions.getPullRequests);
+    app.get("/get_my_repos", checkAuthenticated, api_interactions.getMyRepos);
 
 };
