2c086729102cf1305dec068b41bb6253df828c0c
aimericdrk <aimeric.rouyer@epitech.eu>
2024-12-05 20:39:39 +0100

    :sparkles: new endpoint to list the possible text channel where the bot could send things and list the server where user is admin or owner and bot is + endpoint to get an inviation link

--- backend/src/router/api/api.js
+++ backend/src/router/api/api.js
@@ -1,4 +1,7 @@
 const api_formatter = require("../../middleware/api-formatter.js");
+const axios = require('axios');
+const discordBot = require('../../utils/discord');
+const UserModel = require("../../database/models/users");
 
 // This function will return the user profile
 exports.profile = async (req, res) => {
@@ -16,3 +19,47 @@ exports.profile = async (req, res) => {
         return api_formatter(req, res, 500, "error", "An error occured while trying to get the user profile", null, err, null);
     }
 };
+
+exports.inviteDiscordBot = async (req, res) => {
+    const discordClientId = discordBot.user.id;
+    const inviteLink = `https://discord.com/api/oauth2/authorize?client_id=${discordClientId}&permissions=8&scope=bot%20applications.commands`;
+    return api_formatter(req, res, 200, "success", "discord invitation link getted with success", inviteLink, null, null); // return the user informations
+}
+
+
+// This function will return the discord server where the user is admin
+exports.getDiscordServer = async (req, res) => {
+    try {
+        console.log(req.guilds);
+        const matchingGuilds = req.guilds.map(guild => ({
+            id: guild.id,
+            name: guild.name,
+            icon: guild.icon
+        }));
+        return api_formatter(req, res, 200, "success", "you are authenticated", matchingGuilds, null, null); // return the user informations
+    } catch (err) { // if an error occured
+        return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord server", null, err, null);
+    }
+}
+
+exports.getListOfChannels = async (req, res) => {
+    try {
+        const { guildId } = req.query;
+        if (!guildId)
+            return api_formatter(req, res, 400, "error", "guildId is required", null, null, null);
+        //check if the guildId is in req.quilds
+        const guild = req.guilds.find(guild => guild.id === guildId);
+        if (!guild)
+            return api_formatter(req, res, 400, "error", "you are not admin of this guild or the bot is not present there", null, null, null);
+        // use discordBot to get the text channels of the guild
+        const allChannels = discordBot.guilds.cache.get(guildId).channels.cache;
+        const textChannels = allChannels.filter(channel => channel.type === 0);
+        const channelsList = textChannels.map(channel => ({
+            id: channel.id,
+            name: channel.name,
+        }));
+        return api_formatter(req, res, 200, "success", "you are authenticated", channelsList, null, null); // return the user informations
+    } catch (err) {
+        return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord server", null, err, null);
+    }
+}
--- backend/src/router/api/api.query.js
+++ backend/src/router/api/api.query.js
@@ -1,6 +1,12 @@
 const api_interactions = require("./api.js");
 const checkAuthenticated = require("../../middleware/auth.js");
+const discordAuth = require("../../middleware/discord-auth.js");
 
 module.exports = function (app) {
     app.get("/profile_info", checkAuthenticated, api_interactions.profile);
+    app.get("/invite_discord_bot", api_interactions.inviteDiscordBot);
+
+    app.get("/get_my_discord_server", discordAuth, api_interactions.getDiscordServer);
+    app.get("/get_list_of_channels", discordAuth, api_interactions.getListOfChannels);
+
 };
