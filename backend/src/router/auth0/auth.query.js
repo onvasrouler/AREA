88747d347827b131a09aa905e7b78c39487fe5d3
aimericdrk <aimeric.rouyer@epitech.eu>
2024-12-06 17:31:26 +0100

    :goal_net: adding authentification in order for the new endpoint to work

--- backend/src/router/auth0/auth.query.js
+++ backend/src/router/auth0/auth.query.js
@@ -3,8 +3,8 @@ const checkAuthenticated = require("../../middleware/auth.js");
 
 module.exports = function (app) {
 
-    app.post("/auth/callback/discord", authInteractions.discordCallback);
-    app.post("/auth/refresh/discord", authInteractions.discordRefresh);
+    app.post("/auth/callback/discord", checkAuthenticated, authInteractions.discordCallback);
+    app.post("/auth/refresh/discord", checkAuthenticated, authInteractions.discordRefresh);
 
-    app.get("/auth/callback/github", authInteractions.githubCallback);
+    app.get("/auth/callback/github", checkAuthenticated, authInteractions.githubCallback);
 }
