695c6d455f34c882b306f248f07c05ac1efa7999
aimericdrk <aimeric.rouyer@epitech.eu>
2024-12-09 17:09:46 +0100

    :fire: remove console.log used for debug

--- backend/src/router/auth0/auth.js
+++ backend/src/router/auth0/auth.js
@@ -86,11 +86,9 @@ exports.discordRefresh = async (req, res) => {
 };
 
 exports.githubCallback = async (req, res) => {
-    console.log("githubCallback");
     try {
         const { code } = req.body;
-        console.log(req.body);
-        if (!code)
+=        if (!code)
             return api_formatter(req, res, 400, "error", "code is required", null, null, null);
         try {
             const tokenResponse = await axios.post(
