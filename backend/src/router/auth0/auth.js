726d3f1aec8cfa1c7938cbb22b47c3d2d0e6a225
aimericdrk <aimeric.rouyer@epitech.eu>
2024-12-10 20:29:05 +0100

    :ambulance: hotfix create a new endpoint for discord login and implementing the discord redirect uri mobile

--- backend/src/router/auth0/auth.js
+++ backend/src/router/auth0/auth.js
@@ -85,6 +85,90 @@ exports.discordRefresh = async (req, res) => {
     }
 };
 
+exports.discordCallbackMobile = async (req, res) => {
+    try {
+        const { code } = req.body;
+        if (!code)
+            return api_formatter(req, res, 400, "no code", "code is required when trying to generate a token with the mobile client", null, null, null);
+        try {
+            const params = new URLSearchParams();
+            params.append("client_id", discordBot.user.id);
+            params.append("client_secret", process.env.DISCORD_SECRET);
+            params.append("grant_type", "authorization_code");
+            params.append("code", code);
+            params.append("redirect_uri", process.env.DISCORD_REDIRECT_URI_MOBILE);
+            params.append("scope", "identify guilds");
+
+            const response = await fetch("https://discord.com/api/v10/oauth2/token", {
+                method: "POST",
+                body: params,
+                headers: {
+                    "Content-type": "application/x-www-form-urlencoded"
+                },
+            });
+            let parsedData = await response.json();
+            if (parsedData.access_token) {
+                parsedData.expires_at = Date.now() + (parsedData.expires_in * 1000);
+                req.user.discord_token = parsedData;
+                await req.user.save();
+                return api_formatter(req, res, 200, "success", "Discord token has been saved for the mobile client");
+            } else {
+                console.error(parsedData);
+                return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord token on mobile", null, parsedData);
+            }
+        } catch (error) {
+            console.error(error.response?.data);
+            return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord token on mobile", null, error);
+        }
+    }
+    catch (err) {
+        console.error(err);
+        return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord token on mobile", null, err);
+    }
+};
+
+exports.discordRefreshMobile = async (req, res) => {
+    try {
+        const discord_token = req.user.discord_token;
+        if (!discord_token)
+            return api_formatter(req, res, 400, "error", "you first have to login to discord with the mobile client to be able to refresh your token", null, null, null);
+        const refresh_token = discord_token.refresh_token;
+        if (!refresh_token)
+            return api_formatter(req, res, 400, "error", "you first have to login to discord with the mobile client to be able to refresh your token ", null, null, null);
+        try {
+            const params = new URLSearchParams();
+            params.append("client_id", discordBot.user.id);
+            params.append("client_secret", process.env.DISCORD_SECRET);
+            params.append("grant_type", "refresh_token");
+            params.append("refresh_token", refresh_token);
+            params.append("redirect_uri", process.env.DISCORD_REDIRECT_URI_MOBILE);
+            params.append("scope", "identify guilds");
+
+            const response = await fetch("https://discord.com/api/v10/oauth2/token", {
+                method: "POST",
+                body: params,
+                headers: {
+                    "Content-type": "application/x-www-form-urlencoded"
+                },
+            });
+            const parsedData = await response.json();
+            req.user.discord_token = parsedData;
+            await req.user.save();
+            return api_formatter(req, res, 200, "success", "Discord token has been saved for the mobile client");
+        } catch (error) {
+            console.error(error.response?.data);
+            req.user.discord_token = {};
+            return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord token re log-in discord with the mobile client", null, error);
+
+        }
+    }
+    catch (err) {
+        console.error(err);
+        return api_formatter(req, res, 500, "error", "An error occured while trying to get the discord token with the mobile client", null, err);
+    }
+};
+
+
 exports.githubCallback = async (req, res) => {
     try {
         const { code } = req.body;
--- backend/src/router/auth0/auth.query.js
+++ backend/src/router/auth0/auth.query.js
@@ -6,6 +6,9 @@ module.exports = function (app) {
     app.post("/auth/callback/discord", checkAuthenticated, authInteractions.discordCallback);
     app.post("/auth/refresh/discord", checkAuthenticated, authInteractions.discordRefresh);
 
+    app.post("/mobileauth/callback/discord", checkAuthenticated, authInteractions.discordCallbackMobile);
+    app.post("/mobileauth/refresh/discord", checkAuthenticated, authInteractions.discordRefreshMobile);
+
     app.post("/auth/callback/github", checkAuthenticated, authInteractions.githubCallback);
     app.post("/auth/refresh/github", checkAuthenticated, authInteractions.githubRefresh);
 };
