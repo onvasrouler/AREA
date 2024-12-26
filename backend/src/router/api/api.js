20e84156c324c22bbe75e3904aef4cd47b6eb772
aimericdrk <aimeric.rouyer@epitech.eu>
2024-12-26 20:50:07 +0100

    :rotating_light: fix linter error

--- backend/src/router/api/api.js
+++ backend/src/router/api/api.js
@@ -149,7 +149,7 @@ exports.manage_discord = async (req, res) => {
         const code = req.query.code;
 
         if (!code)
-            return api_formatter(req, res, 400, "Missing code", "you didn't provide any code", null, error, null);
+            return api_formatter(req, res, 400, "Missing code", "you didn't provide any code");
 
         return res.redirect(`${process.env.MOBILE_APP_NAME}://discord?code=${code}`);
     } catch (error) {
