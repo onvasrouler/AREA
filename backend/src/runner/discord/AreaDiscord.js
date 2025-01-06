0683f565d227112bce0926566dc9be2766c57185
aimericdrk <aimeric.rouyer@epitech.eu>
2025-01-06 13:07:04 -0400

    :sparkles: data are formatted before being sended

--- backend/src/runner/discord/AreaDiscord.js
+++ backend/src/runner/discord/AreaDiscord.js
@@ -5,9 +5,29 @@ async function ActionDiscord(AREA) {
     console.log("Treating Discord Action : " + AREA.unique_id);
 }
 
+async function extractData(AREA) {
+    if (AREA.Action.service == "spotify") {
+        if (AREA.Action.arguments.on == "currently_playing") {
+            return AREA.CachedData.item.name + " by " + AREA.CachedData.item.artists[0].name + " : " + AREA.CachedData.item.external_urls.spotify;
+        }
+        if (AREA.Action.arguments.on == "liked_track") {
+            let Datas = "";
+            AREA.CachedData.items.slice(0, 10).forEach(data => {
+                Datas += data.track.name + " by " + data.track.artists[0].name + "\n";
+            });
+            return Datas;
+        }
+        if (AREA.Action.arguments.on == "new_liked_track") {
+            return AREA.CachedData.items[0].track.name + " by " + AREA.CachedData.items[0].track.artists[0].name + " : " + AREA.CachedData.items[0].track.external_urls.spotify;
+        }
+    }
+    return "";
+}
+
 async function ReactionDiscord(AREA) {
     const actionReactions = await ActionReactionModel.findOne({ "unique_id": AREA.unique_id });
     try {
+
         if (!actionReactions) {
             console.error("ActionReaction not found");
             return;
@@ -24,6 +44,8 @@ async function ReactionDiscord(AREA) {
         }
         if (actionReactions.CachedData == "error")
             return;
+        const Datas = await extractData(actionReactions);
+
         const Arguments = actionReactions.Reaction.arguments;
         if (Arguments.react == "message") {
             if (Arguments.channel == null) {
@@ -34,7 +56,7 @@ async function ReactionDiscord(AREA) {
                 console.error("Message is missing");
                 return;
             }
-            await bot.channels.cache.get(Arguments.channel).send(Arguments.message);
+            await bot.channels.cache.get(Arguments.channel).send(Arguments.message + Datas);
         } else if (Arguments.react == "private_message") {
             if (Arguments.userId == null) {
                 console.error("userId is missing");
@@ -46,7 +68,7 @@ async function ReactionDiscord(AREA) {
             }
             await bot.users.fetch(Arguments.userId)
                 .then(user => {
-                    return user.send(Arguments.message);
+                    return user.send(Arguments.message + Datas);
                 })
                 .catch(error => {
                     console.error("Error sending message:", error);
@@ -58,6 +80,7 @@ async function ReactionDiscord(AREA) {
     } catch (err) {
         console.error(err);
         console.error(err.response?.data);
+        console.error("an error occured with area : " + actionReactions)
         return;
     }
 
