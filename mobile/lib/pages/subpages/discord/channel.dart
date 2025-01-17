160676f61467f0b6133e91519bca38590dc814db
Neiluge <julien.renard@epitech.eu>
2025-01-17 11:49:02 +0100

    :sparkles:remove auth services for subpages

--- mobile/lib/pages/subpages/discord/channel.dart
+++ mobile/lib/pages/subpages/discord/channel.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:area/constant/constant.dart';
-import 'package:area/provider/auth.service.dart';
 import 'package:area/provider/action.service.dart';
 
 class ChannelPage extends StatefulWidget {
@@ -16,16 +15,6 @@ class _ChannelPageState extends State<ChannelPage> {
   @override
   void initState() {
     super.initState();
-  }
-
-  final authService = AuthService();
-
-  Future<void> _logout() async {
-    final response = await authService.logout();
-
-    if (response) {
-      GoRouter.of(context).push('/login');
-    }
   }
 
   final actionService = ActionService();
@@ -63,37 +52,6 @@ class _ChannelPageState extends State<ChannelPage> {
                             },
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 200.0),
-                          child: PopupMenuButton<String>(
-                            icon: const CircleAvatar(
-                              backgroundColor: Color.fromARGB(255, 225, 220, 216),
-                              radius: 30,
-                            ),
-                            onSelected: (String value) {
-                              if (value == 'logout') {
-                                _logout();
-                              }
-                            },
-                            itemBuilder: (BuildContext context) {
-                              return [
-                                const PopupMenuItem<String>(
-                                  value: 'logout',
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.logout, 
-                                        color: Colors.red
-                                      ),
-                                      Text('Logout'),
-                                    ],
-                                  ),
-                                ),
-                              ];
-                            },
-                            color: const Color.fromARGB(255, 241, 237, 233),
-                          ),
-                        ),
                       ],
                     ),
                   );
@@ -119,6 +77,7 @@ class _ChannelPageState extends State<ChannelPage> {
                             style: const TextStyle(
                               fontSize: 20,
                               fontStyle: FontStyle.italic,
+                              color: Colors.white,
                             ),
                           ),
                         ),
